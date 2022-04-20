d5e0a1f464a94c589c0b4c7e88e2543adcae0597
Vojimir Raovic <voj@norwoodsystems.com>
2022-04-20 18:07:31 +0800

    Fix syntax and typo.

--- src/mod/applications/mod_google_transcribe/mod_google_transcribe.c
+++ src/mod/applications/mod_google_transcribe/mod_google_transcribe.c
@@ -256,7 +256,7 @@ static switch_status_t start_capture(switch_core_session_t *session, switch_medi
 		return SWITCH_STATUS_FALSE;
 	}
 
-	if (bug == NUL)
+	if (bug == NULL)
 	{
 		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "start_capture - call media_bug_add bug == NULL\n");
 	}
@@ -270,7 +270,7 @@ static switch_status_t start_capture(switch_core_session_t *session, switch_medi
 	}
 	if (bug != NUL)
 	{
-		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "start_capture - we added media_bug - read_codec_name: %s write_codec_name: %s\n", bug.read_impl->iananame, bug.write_impl->iananame );
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "start_capture - we added media_bug - read_codec_name: %s write_codec_name: %s\n", bug->read_impl.iananame, bug->write_impl.iananame );
 	}
 	switch_channel_set_private(channel, MY_BUG_NAME, bug);
 
