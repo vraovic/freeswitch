7a719b0bff089db71767a1c3fdab4cf298b133d3
Vojimir Raovic <voj@norwoodsystems.com>
2022-07-16 01:45:33 +0800

    lws - comment out m_audio_buffer; we are sending just text.

--- src/mod/applications/mod_aai_transcription/audio_pipe.cpp
+++ src/mod/applications/mod_aai_transcription/audio_pipe.cpp
@@ -221,7 +221,8 @@ int AudioPipe::lws_callback(struct lws *wsi,
             uint8_t buf[ap->m_metadata.length() + LWS_PRE];
             memcpy(buf + LWS_PRE, ap->m_metadata.c_str(), ap->m_metadata.length());
             int n = ap->m_metadata.length();
-            lwsl_notice("AudioPipe::lws_write: %s, length: %d\n",(buf + LWS_PRE), n); 
+            lwsl_notice("AudioPipe::lws_write: %s, length: %d \n",ap->m_metadata.c_str(), n); 
+            lwsl_notice("AudioPipe::lws_write - sending buf: %s  \n",buf); 
             int m = lws_write(wsi, buf + LWS_PRE, n, LWS_WRITE_TEXT);
             ap->m_metadata.clear();
             if (m < n) {
@@ -242,18 +243,18 @@ int AudioPipe::lws_callback(struct lws *wsi,
         }
 
         // check for audio packets
-        {
-          std::lock_guard<std::mutex> lk(ap->m_audio_mutex);
-          if (ap->m_audio_buffer_write_offset) {
-            size_t datalen = ap->m_audio_buffer_write_offset;
-            int sent = lws_write(wsi, (unsigned char *) ap->m_audio_buffer, datalen, LWS_WRITE_BINARY);
-            if (sent < datalen) {
-              lwsl_err("AudioPipe::lws_service_thread LWS_CALLBACK_CLIENT_WRITEABLE %s attemped to send %lu only sent %d wsi %p..\n", 
-                ap->m_uuid.c_str(), datalen, sent, wsi); 
-            }
-            ap->m_audio_buffer_write_offset = 0;
-          }
-        }
+        // {
+        //   std::lock_guard<std::mutex> lk(ap->m_audio_mutex);
+        //   if (ap->m_audio_buffer_write_offset) {
+        //     size_t datalen = ap->m_audio_buffer_write_offset;
+        //     int sent = lws_write(wsi, (unsigned char *) ap->m_audio_buffer, datalen, LWS_WRITE_BINARY);
+        //     if (sent < datalen) {
+        //       lwsl_err("AudioPipe::lws_service_thread LWS_CALLBACK_CLIENT_WRITEABLE %s attemped to send %lu only sent %d wsi %p..\n", 
+        //         ap->m_uuid.c_str(), datalen, sent, wsi); 
+        //     }
+        //     ap->m_audio_buffer_write_offset = 0;
+        //   }
+        // }
 
         return 0;
       }
