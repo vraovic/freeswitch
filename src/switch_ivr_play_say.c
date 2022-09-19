fd9027e1bd7402ac603d62098d0221d96cdd0b99
Vojimir Raovic <voj@norwoodsystems.com>
2022-09-19 20:38:34 +0800

    Fix bug.

--- src/switch_ivr_play_say.c
+++ src/switch_ivr_play_say.c
@@ -1399,7 +1399,7 @@ SWITCH_DECLARE(switch_status_t) switch_ivr_record_file_event_and_stream(switch_c
 				fh->silence_hits = fh->samplerate * 3 / read_impl.samples_per_packet;
 				switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_WARNING, "Calculate silence_hits:%d, samplerate:%d, read_impl.samples_per_packet:%d\n",fh->silence_hits,fh->samplerate,read_impl.samples_per_packet);
 			}
-			org_silence_hits = fh->silence_hits;
+			// org_silence_hits = fh->silence_hits;
 		}
 	}
 
