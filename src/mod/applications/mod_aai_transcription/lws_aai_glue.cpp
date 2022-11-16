bb07f04ea513a0c69e63d546f5311b6449cb8c5d
Vojimir Raovic <voj@norwoodsystems.com>
2022-11-16 23:11:35 +0800

    7th

--- src/mod/applications/mod_aai_transcription/lws_aai_glue.cpp
+++ src/mod/applications/mod_aai_transcription/lws_aai_glue.cpp
@@ -174,7 +174,8 @@ namespace {
     // strcat(out, out1);
     // char out[MAX_PATH_LEN] = "/v2/realtime/ws?%22sample_rate%22%3A16000%2C%20%22word_boost%22%3A%5B%22Nedlands%22%2C%20%22Tuart%20Hill%22%5D";
     // char out[MAX_PATH_LEN] = "/v2/realtime/ws?%7B%22sample_rate%22%3A16000%2C%20%22word_boost%22%3A%5B%22Nedlands%22%2C%20%22Tuart%20Hill%22%5D%7D";
-    char out[MAX_PATH_LEN] = "/v2/realtime/ws?%7B%7B%22sample_rate%22%3A16000%2C%20%22word_boost%22%3A%5B%22Nedlands%22%2C%20%22Tuart%20Hill%22%5D%7D%7D";
+    // char out[MAX_PATH_LEN] = "/v2/realtime/ws?%7B%7B%22sample_rate%22%3A16000%2C%20%22word_boost%22%3A%5B%22Nedlands%22%2C%20%22Tuart%20Hill%22%5D%7D%7D";
+    char out[MAX_PATH_LEN] = "/v2/realtime/ws?sample_rate%3D16000%2Cword_boost%3A%5B%22Nedlands%22%2C%20%22Tuart%20Hill%22%5D";
 
 
     // char out2[MAX_PATH_LEN] = "/v2/realtime/ws?\"sample_rate\":16000,\"word_boost\":[\"Nedlands\", \"Tuart Hill\"]";
