36add0dbbd746dc22ed829410ab81bb1fa99660b
Vojimir Raovic <voj@norwoodsystems.com>
2022-11-16 22:55:53 +0800

    with curly bracket.

--- src/mod/applications/mod_aai_transcription/lws_aai_glue.cpp
+++ src/mod/applications/mod_aai_transcription/lws_aai_glue.cpp
@@ -173,6 +173,9 @@ namespace {
     // switch_url_encode(in, out1, sizeof(out1));
     // strcat(out, out1);
     // char out[MAX_PATH_LEN] = "/v2/realtime/ws?%22sample_rate%22%3A16000%2C%20%22word_boost%22%3A%5B%22Nedlands%22%2C%20%22Tuart%20Hill%22%5D";
+    char out[MAX_PATH_LEN] = "/v2/realtime/ws?%7B%22sample_rate%22%3A16000%2C%20%22word_boost%22%3A%5B%22Nedlands%22%2C%20%22Tuart%20Hill%22%5D%7D";
+
+
     // char out2[MAX_PATH_LEN] = "/v2/realtime/ws?\"sample_rate\":16000,\"word_boost\":[\"Nedlands\", \"Tuart Hill\"]";
 
     // switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_NOTICE, "aai_data_init - url_encode - in: %s, out1:%s, out:%s, host:%s\n",path,out1,out, host);
