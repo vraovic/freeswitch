3daf43538d64e1739ff0aa015eb31f5afddc65e8
Vojimir Raovic <voj@norwoodsystems.com>
2022-11-21 00:36:20 +0800

    fix bug

--- src/mod/applications/mod_aai_transcription/mod_aai_transcription.c
+++ src/mod/applications/mod_aai_transcription/mod_aai_transcription.c
@@ -247,7 +247,7 @@ SWITCH_STANDARD_API(aai_transcription_function)
 					token = strtok(path1, "&");
 					token1 = strtok(token,"=");
         			next_token = strtok(NULL, "=");
-					sampling = atoi(token1);
+					sampling = atoi(next_token);
 
           			switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_NOTICE, "TOKEN:%s, NEXT_TOKEN:%s, token1: %s, sampling:%d\n",token, next_token,token1,sampling);
           			switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_NOTICE, "start_capture - host:%s,port:%d, path:%s, sampling:%d\n",host,port, path, sampling);
