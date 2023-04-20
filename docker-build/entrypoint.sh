095df47da0bba38b3c376618dc98585b7dbc957a
ljiljana zoric <ljiljana.zoric@norwoodsystems.com>
2023-04-20 22:06:32 +0800

    adding entrypoint script

--- docker-build/entrypoint.sh
+++ docker-build/entrypoint.sh
@@ -3,3 +3,4 @@
 # Save the variable value to a file
 echo "$GOOGLE_APPLICATION_API">>/usr/local/freeswitch/freeswitch-api-7867f30c80d7.json
 
+freeswitch -nonat -conf /usr/local/freeswitch/conf -log /usr/local/freeswitch/log -db /usr/local/freeswitch/db -scripts /usr/local/freeswitch/scripts -storage /usr/local/freeswitch/store
