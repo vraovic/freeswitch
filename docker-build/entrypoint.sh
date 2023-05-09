6050474b6a37367a091776d051cbb1ee989b4ee3
Vojimir Raovic <voj@norwoodsystems.com>
2023-05-09 10:58:57 +0800

    Revert "adding entrypoint script"
    This reverts commit 3a0790645d961874edd0f7312745480b862616df.

--- docker-build/entrypoint.sh
+++ docker-build/entrypoint.sh
@@ -2,7 +2,5 @@
 
 # Save the variable value to a file
 echo "$GOOGLE_APPLICATION_API">>/usr/local/freeswitch/freeswitch-api-7867f30c80d7.json
-chmod 766 /usr/local/freeswitch/freeswitch-api-7867f30c80d7.json
-chown freeswitch:freeswitch /usr/local/freeswitch/freeswitch-api-7867f30c80d7.json
 
 freeswitch -nonat -conf /usr/local/freeswitch/conf -log /usr/local/freeswitch/log -db /usr/local/freeswitch/db -scripts /usr/local/freeswitch/scripts -storage /usr/local/freeswitch/store
